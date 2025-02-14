f7110bb9450fd75967d4d4e4efadeb78f68f59ef
sanjaimadhu <smadhusudhanan63@gmail.com>
2025-02-14 19:42:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/recipes', recipeRoutes);
+app.use("/", (req, res) => {
+    res. json({
+        status: "API IS RUNNING",
+        code: 200
+    })
+})
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
